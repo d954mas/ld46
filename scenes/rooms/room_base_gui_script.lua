aca621cb674b357a5ba0d9afea44a8ae1b7979e1
d954mas <d954mas@gmail.com>
2020-04-18 19:04:41 +0300

    phone is call

--- scenes/rooms/room_base_gui_script.lua
+++ scenes/rooms/room_base_gui_script.lua
@@ -1,6 +1,7 @@
 local COMMON = require "libs.common"
 local WORLD = require "model.world"
 local RoomObjectView = require "model.rooms.room_object_view"
+local ACTIONS = require "libs.actions.actions"
 
 ---@class RoomBaseGUI
 local Script = COMMON.new_n28s()
@@ -22,13 +23,25 @@ function Script:init()
 	self:init_gui()
 	self:bind_room()
 	self:bind_config()
+	self.thread = COMMON.ThreadManager()
+	self.animations = ACTIONS.Sequence()
+	self.animations.drop_empty = false
+	self.thread:add(self.animations)
 
 	for k, object in pairs(self.config.objects) do
 		self.room:get_object_by_id(object.id):set_view(RoomObjectView({ root = gui.get_node(object.id) }))
 	end
+
+	self:init_done()
 end
 
-function Script:update(dt) end
+function Script:init_done()
+
+end
+
+function Script:update(dt)
+	self.thread:update(dt)
+end
 
 function Script:final()
 	COMMON.input_release()
